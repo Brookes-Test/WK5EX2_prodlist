7a37111d7080671656d528ac84e02aca90cafb6f
BrookesDavidS <168030319+BrookesDavidS@users.noreply.github.com>
2024-04-26 15:45:45 +0100

    Create testutils.py

---
+++ testutils.py
@@ -0,0 +1,10 @@
+#IMPORTANT: DO NOT CHANGE THIS FILE!
+import unittest
+from main import prodlist
+
+class TestUtils(unittest.TestCase):
+  def test1(self):
+    self.fail("Test Fail Message")
+
+if __name__ == '__main__':
+    unittest.main()
